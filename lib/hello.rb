6676e2b5283c1bca3e78f877bc75e34fc09125d4
Tiffany <ong.tifffany1503@gmail.com>
2019-05-20 17:41:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,16 +4,11 @@ def hello_t(array)
     yield(array[i])
     i = i + 1
   end
+  array
 end
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i +- 1
-  end
-  collection
-end
-
 # call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
