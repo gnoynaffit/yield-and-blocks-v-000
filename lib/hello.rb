c506ca6bc257a3e99e2701b96d15e6fd5a089e15
Tiffany <ong.tifffany1503@gmail.com>
2019-05-20 17:24:41 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,19 @@
-def hello_t
+def hello_t(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+end
 
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i +- 1
+  end
+  collection
 end
 
 # call your method here!
